003296543ac77c02afb763383f1ab74fc2aa9232
Алексей Иванов <ivanovad97@gmail.com>
2023-09-20 18:04:38 +0500

    Initial Commit

---
+++ BoysVSGirlsSwiftUI/BoysVSGirlsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoysVSGirlsSwiftUIApp.swift
+//  BoysVSGirlsSwiftUI
+//
+//  Created by Алексей on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BoysVSGirlsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
